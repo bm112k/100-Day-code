d80ee4d5f8b33132242762b9404390253faabac5
Bhavya Mahajan <bhavyamahajan303@gmail.com>
2026-08-12 18:09:17 +0530

    Add temp program

---
+++ switch.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+
+a = 3;
+b = 5;
+
+c = a;
+a = b;
+b = c;
+
+printf("a is %d\n b is %d\n  ",a,b);
+
+return 0;
+
+
+}
---
+++ temp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+float c,f;
+
+printf("Enter the temperature in degree celcius :\n");
+scanf("%f",&c);
+
+f = (c*1.8)+32;
+
+printf("The temparature in farenhite is : %f",f);
+
+return 0;
+
+
+}
